502311840b2689995afc597443fc185891971d80
kchantha <thasmile1@gmail.com>
2022-06-26 08:06:59 +0700

    Update slide

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -9,7 +9,26 @@ import SideDrawer from './SideDrawer';
 class Header extends Component{
 
     state={
-        drawerOpen: false
+        drawerOpen: false,
+        headerShow: false
+    }
+
+    componentDidMount(){
+        window.addEventListener('scroll',this.handleScroll);
+    }
+
+
+    handleScroll = () =>{
+        if(window.scrollY > 0){
+            this.setState({
+                headerShow:true
+            })
+        }else{
+            //alert('hi1');
+            this.setState({
+                headerShow: false
+            })
+        }
     }
 
     toggleDrawer = (value)=> {
@@ -23,7 +42,7 @@ class Header extends Component{
             <AppBar 
                 position="fixed"
                 style={{
-                    backgroundColor:'#2f2f2f',
+                    backgroundColor: this.state.headerShow ? '#2f2f2f':'transparent',
                     boxShadow:'none',
                     padding: '10px 0px'
                 }}
